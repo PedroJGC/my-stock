46cc11a30a5da2f7b96af5cc6dcb135d04bc8637
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-12 00:45:31 -0300

    chore: initial commit

---
+++ my-stock-back-end/prisma/migrations/20250812034306_create_products_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
