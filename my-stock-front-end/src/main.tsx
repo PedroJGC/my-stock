ee37039ca60dc22508ef1bf939c230cd5d45fb6f
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-17 12:02:11 -0300

    chore: initial project setup

--- my-stock-front-end/src/main.tsx
+++ my-stock-front-end/src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: Using non-null assertion because container is guaranteed by DOM.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
