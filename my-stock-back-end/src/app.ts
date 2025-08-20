63c7616551b353d2a564b57997882960da8d8adb
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-20 18:21:00 -0300

    feat: add CORS support to the Fastify app

--- my-stock-back-end/src/app.ts
+++ my-stock-back-end/src/app.ts
@@ -3,4 +3,11 @@ import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
+// Registrar o plugin de CORS
+app.register(import('@fastify/cors'), {
+  origin: ['http://localhost:5173'], // URL do frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+})
+
 app.register(appRoutes)
